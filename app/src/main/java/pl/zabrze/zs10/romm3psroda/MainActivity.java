93f828904269e0ed3870b337847c07211a2ed699
markup54 <marzenakupka@tlen.pl>
2024-04-10 17:41:54 +0200

    zwracanie wszystkich todo

--- app/src/main/java/pl/zabrze/zs10/romm3psroda/MainActivity.java
+++ app/src/main/java/pl/zabrze/zs10/romm3psroda/MainActivity.java
@@ -7,9 +7,21 @@ import androidx.room.RoomDatabase;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
     TodoDatabase todoDB;
+    List<Todo> todoList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +41,57 @@ public class MainActivity extends AppCompatActivity {
                 TodoDatabase.class, "todo_db")
                 .addCallback(myCallback)
                 .build();
-
+        Button button = findViewById(R.id.button);
+        EditText editText =findViewById(R.id.editTextTodo);
+        button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dodajTodoWTle(new Todo(editText.getText().toString()));
+                    }
+                }
+        );
 
     }
+
+    public void wypiszWszystkoZbazy(){
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+        executorService.execute(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                       todoList = todoDB.getTodoDAO().getAllTodo();
+                       handler.post(new Runnable() {
+                           @Override
+                           public void run() {
+                             //TODO: aktulizować adapter do listView
+                           }
+                       });
+                    }
+                }
+        );
+    }
+    public void dodajTodoWTle(Todo todo){
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+        executorService.execute(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        todoDB.getTodoDAO().insertTodo(todo);
+                        handler.post(
+                                new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(MainActivity.this, "dodano do bazy", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                        );
+                    }
+                }
+        );
+    }
+
+
 }
